76dfb3ef661863a231d03be2d8719eef94e8df0d
Mahmoud <mahmoud.ped@gmail.com>
2023-12-28 11:45:56 +0200

    1.2

---
+++ src/actions/index.ts
@@ -0,0 +1,13 @@
+'use server'
+
+
+import * as auth from '@/auth'
+
+export async function signIn() {
+    return auth.signIn('github')
+  }
+
+
+  export async function signOut() {
+    return auth.signOut()
+  }
