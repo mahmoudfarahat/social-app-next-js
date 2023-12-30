a86c8ced008d59d057338907debf8ab47631ae0d
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-12-30 03:22:24 +0200

    1.2

--- src/actions/create.topic.ts
+++ src/actions/create.topic.ts
@@ -1,21 +1,31 @@
 'use server'
-
+import type { Topic } from '@prisma/client';
+import {redirect} from 'next/navigation'
 import {z} from 'zod';
+import {auth} from '@/auth';
+import {db} from '@/db'
+import paths from '@/path'
+import { revalidatePath } from 'next/cache';
 
-const createTopicSchema= z.object({
+ const createTopicSchema= z.object({
     name:z.string().min(3).regex(/^[a-z-]+$/,{message:"Must be lowercase letters or dashes without spaces"}),
     description: z.string().min(10)
 })
 
 interface CreateTopicFormState{
     errors:{
-        name?:string[],
-        description?:string[]
+        name?:string[];
+        description?:string[];
+        _form?:string[];
+        
     }
 }
 
 export async function createTopic(formState :CreateTopicFormState ,formData:FormData) :Promise<CreateTopicFormState>{
 
+
+
+//await new Promise(resolve => setTimeout(resolve, 2500))    
     const result = createTopicSchema.safeParse({
         name: formData.get('name'),
         description : formData.get('description')
@@ -27,7 +37,41 @@ export async function createTopic(formState :CreateTopicFormState ,formData:Form
     }
     }
 
- return  {
-    errors :{}
- }
+    const session = await auth();
+    if(!session || !session.user){
+        return {
+            errors:{
+                _form:['You must be signed in to do that']
+            }
+        }
+    }
+    let topic :Topic;
+    try{
+    topic = await db.topic.create({
+    data:{
+        slug:result.data.name,
+        description :result.data.description
+    }
+})
+    }catch(err:unknown){
+if(err instanceof Error){
+    return {
+        errors:{
+            _form:[err.message]
+        }
+    }
+}else{
+    return {
+        errors:{
+            _form:['Something went wrong']
+        }
+    }
+}
+
+    }
+    revalidatePath('/')
+    redirect(paths.topicShow(topic.slug))
+    
+ 
+ 
 }
--- src/components/posts/post.create.form.tsx
+++ src/components/posts/post.create.form.tsx
@@ -11,26 +11,32 @@ import {
 } from '@nextui-org/react'
 import * as actions from '@/actions'
 import FormButton from '@/components/common/form-button'
-export default function TopicCreateForm(){
-    const [formState,action] = useFormState(actions.createTopic,{
+
+interface PostCreateFromProps{
+    slug:string
+}
+export default function PostCreateForm({slug}:PostCreateFromProps){
+    const [formState,action] = useFormState(actions.createPost.bind(null,slug),{
             errors:{}
     })
+
+
     return (
             <Popover placement="left">
                         <PopoverTrigger>
-                            <Button color='primary'>Create a Topic</Button>
+                            <Button color='primary'>Create a Post</Button>
                         </PopoverTrigger>   
                         <PopoverContent>
                             <form action={action}>
                                 <div className='flex flex-col gap-4 p-4 w-80'>
-                                    <h3 className="text-lg">Create a Topic</h3>
-                                    <Input name="name" label="Name" labelPlacement='outside' placeholder='Name' 
-                                    isInvalid={!!formState.errors.name}
-                                    errorMessage={formState.errors.name?.join(', ')} /> 
+                                    <h3 className="text-lg">Create a Post</h3>
+                                    <Input name="title" label="Title" labelPlacement='outside' placeholder='title' 
+                                    isInvalid={!!formState.errors.title}
+                                    errorMessage={formState.errors.title?.join(', ')} /> 
                                
-                                    <Textarea   name="description" label="Description" labelPlacement="outside" placeholder="Describe your topic" 
-                                       isInvalid={!!formState.errors.description}
-                                       errorMessage={formState.errors.description?.join(', ')}/>
+                                    <Textarea   name="content" label="Content" labelPlacement="outside" placeholder="Describe your content" 
+                                       isInvalid={!!formState.errors.content}
+                                       errorMessage={formState.errors.content?.join(', ')}/>
 
 
                                        {formState.errors._form? <div className="rounded p-2 bg-red-200 border border-red-40">{formState.errors._form?.join(', ')}</div> : null}
